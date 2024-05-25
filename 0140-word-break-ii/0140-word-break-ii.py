3e0ffa37bda393e37a7ade11fa7ec6c121e2bfd4
saikiran renikunta <168349505+saikiranrenikunta007@users.noreply.github.com>
2024-05-25 19:54:56 +0530

    Time: 41 ms (15.64%), Space: 16.5 MB (43.69%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,10 +1,8 @@
 class Solution:   
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         d = set(wordDict)
-        # print(d)
         memo={}
         def solve(s,d):
-            # print(s)
             if s in memo :
                 return memo[s]
             if len(s) == 0:
@@ -14,13 +12,11 @@ class Solution:
                 if s[i:] in d:
                     l = solve(s[0:i],d)
                     for x in l:
-                        # print(x)
                         memo[s]+=[x+[s[i:]]]
             return memo[s]
         a = solve(s,d)
         ans=[]
         for i in a:
-            # print(i)
             ans.append(" ".join(i))
         return ans
             
