375bb80c1515181154365b1527c8b7bc1d121a85
saikiran renikunta <168349505+saikiranrenikunta007@users.noreply.github.com>
2024-05-25 20:22:37 +0530

    Time: 36 ms (47.42%), Space: 16.5 MB (87.62%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -6,18 +6,20 @@ class Solution:
             if s in memo :
                 return memo[s]
             if len(s) == 0:
-                return [[]]
+                return []
             memo[s] = []
             for i in range(len(s)-1,-1,-1):
                 if s[i:] in d:
                     l = solve(s[0:i],d)
+                    # print("l=",l)
+                    l2=[]
                     for x in l:
-                        memo[s]+=[x+[s[i:]]]
+                        ash = x+" "+s[i:]
+                        l2.append(ash)
+                    if(len(l2)==0 and i==0):
+                        l2 = [s[i:]]
+                    memo[s]+=l2
             return memo[s]
-        a = solve(s,d)
-        ans=[]
-        for i in a:
-            ans.append(" ".join(i))
-        return ans
+        return solve(s,d)
             
         
